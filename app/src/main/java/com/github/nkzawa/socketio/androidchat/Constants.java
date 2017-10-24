ee7dd5033b8712e366665dcf34f55aeea285e5ad
yallw <marco.argentieri@hotmail.com>
2017-10-23 19:39:52 -0700

    revert chat server URL

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,5 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "http://copycatserver.azurewebsites.net/" ;
+    public static final String CHAT_SERVER_URL = "http://copycatserver.azurewebsites.net/";
 }
