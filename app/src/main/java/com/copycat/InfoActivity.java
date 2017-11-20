440ed84aca9a23cb8ff3d2748c1d333f8760da60
caceresdaniel <mattmoocow@live.com>
2017-11-19 18:39:05 -0800

    changes to info activity

--- app/src/main/java/com/copycat/InfoActivity.java
+++ app/src/main/java/com/copycat/InfoActivity.java
@@ -5,17 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import autovalue.shaded.com.google$.common.collect.$Iterables;
+
 public class InfoActivity extends AppCompatActivity {
 
     private TextView mainTitle;
+    private TextView aboutTitle;
+    private TextView patchTitle;
+    private TextView howTitle;;
+    private TextView moreTitle;
+    private TextView genTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        mainTitle = (TextView) findViewById(R.id.infoHeader);
         Typeface font = Typeface.createFromAsset(getAssets(),"fonts/Quantify Bold v2.6.ttf");
+
+        mainTitle = (TextView) findViewById(R.id.infoHeader);
         mainTitle.setTypeface(font);
+
+        aboutTitle = (TextView) findViewById(R.id.textView3);
+        aboutTitle.setTypeface(font);
+
+        patchTitle = (TextView) findViewById(R.id.textView6);
+        patchTitle.setTypeface(font);
+
+        howTitle = (TextView) findViewById(R.id.textView7);
+        howTitle.setTypeface(font);
+
+        moreTitle = (TextView) findViewById(R.id.textView8);
+        moreTitle.setTypeface(font);
+
+        genTitle = (TextView) findViewById(R.id.genTitle);
+        genTitle.setTypeface(font);
     }
 }
